312aeea295085d61f2842af33dd46b5499d0639b
iutgeiitoulonE105 <touloniut@gmail.com>
2024-12-18 17:32:24 +0100

    18/12
    interface graphique plus envoi sur le robot avec retour que l'on peut observer à l'oscillo
    on reste en bin

--- C#/RobotInterface/MainWindow.xaml.cs
+++ C#/RobotInterface/MainWindow.xaml.cs
@@ -8,43 +8,57 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExtendedSerialPort_NS;
+using System.IO.Ports;
 
-namespace RobotInterface
+
+
+namespace RobotInterface;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    
+    ExtendedSerialPort serialPort1;
+    
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        serialPort1 = new ExtendedSerialPort("COM7", 115200, Parity.None, 8, StopBits.One);
+        serialPort1.Open();
+    }
+    void SendMessage()
+    {
+        textBoxReception.Text += ("Reçu :" + textBoxEmission.Text + "\n");
+        textBoxEmission.Text = null;
+    }
+    private void TextBoxEmission_KeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
         {
-            InitializeComponent();
-        }
-
-        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
-        {
-
             SendMessage();
-            if (buttonEnvoyer.Background == Brushes.Beige)
-            {
-                buttonEnvoyer.Background = Brushes.RoyalBlue;
-            }
-            else
-            {
-                buttonEnvoyer.Background = Brushes.Beige;
-            }
         }
-        private void SendMessage()
+    }
+    private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+    {
+
+        SendMessage();
+        if (buttonEnvoyer.Background == Brushes.Beige)
         {
-            textBoxReception.Text += ("Reçu :" + textBoxEmission.Text + "\n");
-            textBoxEmission.Text = null;
+            buttonEnvoyer.Background = Brushes.RoyalBlue;
         }
-        private void TextBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        else
         {
-            if (e.Key == Key.Enter)
-            {
-                SendMessage();
-            }
+            buttonEnvoyer.Background = Brushes.Beige;
         }
+    }
+    
+    
+
+    private void textBoxEmission_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
     }
 }
