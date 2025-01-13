ce7653285bd3b29c3346ff023ef2111caaf28973
iutgeiitoulonE105 <touloniut@gmail.com>
2025-01-13 11:15:16 +0100

    interface et msgs ok

--- C#/RobotInterface/MainWindow.xaml.cs
+++ C#/RobotInterface/MainWindow.xaml.cs
@@ -10,11 +10,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ExtendedSerialPort_NS;
 using System.IO.Ports;
+using System.Printing;
+using System.Windows.Threading;
 
 
 
 namespace RobotInterface;
-
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
@@ -22,18 +23,62 @@ public partial class MainWindow : Window
 {
     
     ExtendedSerialPort serialPort1;
-    
+    string receivedText;
+    DispatcherTimer timerAffichage;
+    /// <summary>
+    /// Init timer affichage
+    /// </summary>
+    Robot robot = new Robot();
+
     public MainWindow()
     {
         InitializeComponent();
         serialPort1 = new ExtendedSerialPort("COM5", 115200, Parity.None, 8, StopBits.One);
         serialPort1.Open();
+        serialPort1.DataReceived += SerialPort1_DataReceived;
+
+        timerAffichage = new DispatcherTimer();
+        timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+        timerAffichage.Tick += TimerAffichage_Tick;
+        timerAffichage.Start();
+
     }
+
+    private void TimerAffichage_Tick(object? sender, EventArgs e)
+    {
+        textBoxReception.Text += receivedText;
+        receivedText = "";
+    }
+
     void SendMessage()
     {
-        textBoxReception.Text += ("Reçu :" + textBoxEmission.Text + "\n");
+        //textBoxReception.Text += ("Reçu :" + textBoxEmission.Text + "\n");
+        serialPort1.WriteLine(textBoxEmission.Text);
+        textBoxEmission.Text = null;      
+        
+    }
+    void Clear()
+    {
+        textBoxReception.Text = null;
+    }
+    void Test()
+    {
+        textBoxReception.Text += ("Reçu :" + "Test" + "\n");
         textBoxEmission.Text = null;
     }
+
+    public void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+    {
+        receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+    }
+
+
+
+
+
+
+
+
     private void TextBoxEmission_KeyUp(object sender, KeyEventArgs e)
     {
         if (e.Key == Key.Enter)
@@ -41,6 +86,7 @@ public partial class MainWindow : Window
             SendMessage();
         }
     }
+
     private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
     {
 
@@ -54,8 +100,19 @@ public partial class MainWindow : Window
             buttonEnvoyer.Background = Brushes.Beige;
         }
     }
-    
-    
+
+    private void buttonClear_Click(object sender, RoutedEventArgs e)
+    {
+
+        Clear();
+    }
+
+    private void buttonTest_Click(object sender, RoutedEventArgs e)
+    {
+        Test();
+        Clear();
+    }
+
 
     private void textBoxEmission_TextChanged(object sender, TextChangedEventArgs e)
     {
